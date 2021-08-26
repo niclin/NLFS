6d62638de73110da4cff641c0e98aa5bf26d18d5
Nic <niclin0226@gmail.com>
2021-08-26 14:51:04 +0800

    Add schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_26_063350) do
+ActiveRecord::Schema.define(version: 2021_08_26_063720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2021_08_26_063350) do
     t.integer "status", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "moto_type"
   end
 
   create_table "users", force: :cascade do |t|
