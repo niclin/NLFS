524e884790d80e69e1292f7a15d68abe0b29597d
Nic <niclin0226@gmail.com>
2021-08-26 14:40:14 +0800

    Fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
 
   def show
     @post_attachments = @post.post_attachments.all
-    set_meta_tags title: "[便友帥車] #{@post.title}",
+    set_meta_tags title: @post.title,
                   description: @post.comment,
                   keywords: "#{@post.store}, #{@post.front_brake_caliper}, #{@post.front_wheel_rim}, #{@post.front_shock}, #{@post.back_brake_caliper}, #{@post.back_wheel_rim}, #{@post.back_shock}",
                   og: {
