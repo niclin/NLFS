cc92cbe4ef1bf932c34252e9038f3675b5ad3965
Nic <niclin0226@gmail.com>
2021-08-26 17:26:51 +0800

    Show status in preview

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,28 @@
 module PostsHelper
+
+  def render_preview_title(post)
+    case post.status
+    when "draft"
+      "草稿"
+    when "submitted"
+      "已送出"
+    when "approved"
+      "審核通過"
+    else
+      "ERROR"
+    end
+  end
+
+  def render_preview_bar_css_color(post)
+    case post.status
+    when "draft"
+      "bg-secondary"
+    when "submitted"
+      "bg-warning"
+    when "approved"
+      "bg-warning"
+    else
+      "bg-info"
+    end
+  end
 end
